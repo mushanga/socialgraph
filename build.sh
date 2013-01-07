2e2b7732e19406755738d293f98832c6d35b9492
cagdas <matkap@gmail.com>
2013-01-07 06:00:22 +0100

    eaca

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 git pull
 mvn clean install
 service tomcat6 stop
-<<<<<<< HEAD
 rm -rf /var/lib/tomcat6/webapps/debatree
-=======
-rm -rf /var/lib/tomcat6/webapps/tcommerce
->>>>>>> 5d45ee488312868890927b23e091a7b5a389434a
 cp /root/debatree/target/debatree-1.0-SNAPSHOT.war /var/lib/tomcat6/webapps/socialgraph.war
 service tomcat6 start
