ceae3f6b6666b923241665b040dfec5093d417dd
cagdas <matkap@gmail.com>
2013-01-07 06:33:45 +0100

    sefs

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git pull origin master
 mvn clean install
 service tomcat6 stop
-rm -rf /var/lib/tomcat6/webapps/debatree
+rm -rf /var/lib/tomcat6/webapps/socialgraph
 cp /root/debatree/target/debatree-1.0-SNAPSHOT.war /var/lib/tomcat6/webapps/socialgraph.war
 service tomcat6 start
