8e73a680cc20e18ee1a4f9313c79edc8a622f00c
cagdas <matkap@gmail.com>
2013-01-10 23:29:06 +0100

    hibernate exception handling

--- src/main/java/com/debatree/service/UserServiceImpl.java
+++ src/main/java/com/debatree/service/UserServiceImpl.java
@@ -2,8 +2,9 @@ package com.debatree.service;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
-import org.hibernate.Query;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
@@ -11,32 +12,59 @@ import com.amazonbird.util.Util;
 import com.debatree.data.User;
 
 public class UserServiceImpl {
-	
+
+	private static Logger logger = Logger.getLogger(UserServiceImpl.class);
 	public void saveUser(User user) {
 		Session session = HibernateUtil.getSession();
-		session.beginTransaction();
-		session.save(user);
-		session.getTransaction().commit();
-		session.close();
+	
+		try {
+			session.beginTransaction();
+			session.save(user);
+			session.getTransaction().commit();
+		
+		} catch (HibernateException e) {
+			logger.error(e.getMessage(),e);
+		}finally{
+			session.close();
+		}
 	}
 
-	public User getUser(long id) {
+	public User getUser(long id){
+		
 		Session session = HibernateUtil.getSession();
-		User t = (User) session.get(User.class, new Long(id));
-	
-		session.close();
-		return t;
+		
+		try {
+			User t = (User) session.get(User.class, new Long(id));
+
+			return t;
+		} catch (HibernateException e) {
+			
+			logger.error(e.getMessage(),e);
+		}finally{
+
+			session.close();
+		}
+		return null;
 	}
 	public User getUserByName(String name) {
 		Session session = HibernateUtil.getSession();
-		Criteria criteria = session.createCriteria(User.class);
-		List results = criteria.add(Restrictions.eq("screenName", name)).list();
 		
-		if(Util.getInstance().isListValid(results)){
-			return (User) results.get(0);
-		}else{
-			return null;
+		try {
+			Criteria criteria = session.createCriteria(User.class);
+			List results = criteria.add(Restrictions.eq("screenName", name)).list();
+			
+			if(Util.getInstance().isListValid(results)){
+				return (User) results.get(0);
+			}else{
+				return null;
+			}
+		} catch (HibernateException e) {
+			logger.error(e.getMessage(),e);
+		}finally{
+
+			session.close();
 		}
+		return null;
 		
 	}
 
